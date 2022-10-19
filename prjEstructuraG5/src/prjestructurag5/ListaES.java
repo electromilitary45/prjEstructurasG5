b7f33fa4bb1a541ed8adfeaa7db627d44e5ecd62
Marcel1203 <Mazaca120703@gmail.com>
2022-10-19 16:19:38 -0600

    Metodo de desactivar y posible metodo de agregar
    metodo de desactivar y un intento de agregar.

--- prjEstructuraG5/src/prjestructurag5/ListaES.java
+++ prjEstructuraG5/src/prjestructurag5/ListaES.java
@@ -2,8 +2,6 @@ package prjestructurag5;
 
 import javax.swing.JOptionPane;
 
-
-
 public class ListaES {
 
     private Nodo inicio;
@@ -19,19 +17,67 @@ public class ListaES {
             return false;
         }
     }//fin Vacia()
-    
+
     //-----------------------------METODOS---------------------------------
-    public void agregarUsuario(String nombre, String apellido, String usuario, String contrasena){
-        JOptionPane.showMessageDialog(null, usuario+"-"+apellido+"-"+usuario+"-"+contrasena);
+    public void agregarUsuario(String nombre, String apellido, String usuario, String contrasena) {
+        JOptionPane.showMessageDialog(null, usuario + "-" + apellido + "-" + usuario + "-" + contrasena);
+        Dato d = new Dato();
+        d.setNombre(nombre);
+        d.setApellido(apellido);
+        d.setUsuario(usuario);
+        d.setContrasena(contrasena);
+
+        Nodo nuevo = new Nodo();
+        nuevo.setElemento(d);
+        if (VaciasLista()) {
+            inicio = nuevo;
+        } else if (d.getNombre().compareTo(inicio.getElemento().getNombre()) < 0) {
+            nuevo.setSiguiente(inicio);
+            inicio = nuevo;
+        } else if (inicio.getSiguiente() == null) {
+            inicio.setSiguiente(nuevo);
+        } else {
+            Nodo aux = inicio;
+            while ((aux.getSiguiente() != null) && (aux.getSiguiente().getElemento().getNombre().
+                    compareTo(d.getNombre()) < 0)) {
+                aux = aux.getSiguiente();
+            }
+            nuevo.setSiguiente(aux.getSiguiente());
+            aux.setSiguiente(nuevo);
+        }
     }//fin agregarUsuario
-    
-    public void desactivarUsuario(String usuario){
-        JOptionPane.showMessageDialog(null, usuario);
+
+    public void desactivarUsuario(String usuario) {
+        //JOptionPane.showMessageDialog(null, usuario);
+        if (!VaciasLista()) {
+            String nomb = JOptionPane.showInputDialog(null,
+                    "Digite el nombre del cliente a desactivar:");
+            if (inicio.getElemento().getNombre().equals(nomb)) {
+                inicio = inicio.getSiguiente();
+                JOptionPane.showMessageDialog(null,
+                        "¡Se desactivo el cliente con exito!");
+            } else {
+                Nodo anterior;
+                Nodo auxiliar;
+                anterior = inicio;
+                auxiliar = inicio.getSiguiente();
+                while ((auxiliar != null) && (!auxiliar.getElemento()
+                        .getNombre().equals(nomb))) {
+                    anterior = anterior.getSiguiente();
+                    auxiliar = auxiliar.getSiguiente();
+                }
+                if (auxiliar != null) {
+                    anterior.setSiguiente(auxiliar.getSiguiente());
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "La lista esta vacia");
+        }
     }//fin desactivar
-    
-    public void mostrarUsuarios(){
+
+    public void mostrarUsuarios() {
         JOptionPane.showMessageDialog(null, "AQUI SE MOSTRARAN LOS USUARIOS");
     }
-    
-    
+
 }//fin clase LISTAES(){}
