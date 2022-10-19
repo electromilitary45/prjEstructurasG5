df7a69af23821a0c9cd222c97f341f07f79d135e
Aaron Bustos <aaronj.bustos@gmail.com>
2022-10-18 18:02:37 -0600

    agegue los atributos de datos

--- prjEstructurasG5/ProyectoEstrucDatos/src/proyectoestrucdatos/Datos.java
+++ prjEstructurasG5/ProyectoEstrucDatos/src/proyectoestrucdatos/Datos.java
@@ -1,4 +1,16 @@
 package proyectoestrucdatos;
 public class Datos {
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String clave; //Mi teclado fisico esta en ingles
+    private boolean estado;
     
+    public Datos(){
+        this.nombre="";
+        this.apellido="";
+        this.usuario="";
+        this.clave="";
+        this.estado=true; //false o true 
+    }
 }
